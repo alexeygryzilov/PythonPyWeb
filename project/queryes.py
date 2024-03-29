25ab1581b332d3acda8313177ffbfe23c6c83dba
Alexey <alexeygryzilov@gmail.com>
2024-03-29 15:38:06 +0300

    Lab_2

--- project/queryes.py
+++ project/queryes.py
@@ -1,7 +1,9 @@
 import django
 import os
 import datetime
-from django.db.models import Count
+from django.db.models import Count, Max, Min, Sum
+from django.db.models import Avg, Q, StdDev, Variance
+from django.db import connection
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 django.setup()
@@ -30,6 +32,8 @@ if __name__ == "__main__":
     # print(Entry.objects.filter(headline__endswith='ния'))
     start_date = datetime.date(2023, 1, 1)
     end_date = datetime.date(2023, 12, 31)
+
+
     # print(Entry.objects.filter(pub_date__range=(start_date, end_date)))
     # print(Entry.objects.filter(pub_date__year=2023))
     # print(Entry.objects.filter(pub_date__year__lt=2022))
@@ -64,11 +68,82 @@ if __name__ == "__main__":
     # except Blog.DoesNotExist:
     #   print("Не существует")
     # print(Blog.objects.filter(id=1, name="Путешествия по миру").exists())
-    #f_data = Blog.objects.filter(id__gte=2)
-    #print(f_data.order_by('-name', '-id'))
-    #entry = Blog.objects.annotate(number_of_entries=Count('entries')).values('name', 'number_of_entries')
-    #print(entry)
-    #blogs = Blog.objects.alias(number_of_entries=Count('entries')).filter(number_of_entries__gt=4)
-    #print(blogs)
-    #blogs = Blog.objects.alias(number_of_entries_new=Count('entries')).filter(number_of_entries__gt=4).values('blog', 'entries_new')
+    # f_data = Blog.objects.filter(id__gte=2)
+    # print(f_data.order_by('-name', '-id'))
+    # entry = Blog.objects.annotate(number_of_entries=Count('entries')).values('name', 'number_of_entries')
+    # print(entry)
+    # blogs = Blog.objects.alias(number_of_entries=Count('entries')).filter(number_of_entries__gt=4)
+    # print(blogs)
+    # blogs = Blog.objects.alias(number_of_entries_new=Count('entries')).filter(number_of_entries__gt=4).values('blog', 'entries_new')
+    # average_rating = Entry.objects.aggregate(
+    #   average_rating1=Avg('rating', distinct=True))
+    # print(average_rating)
+    # average_rating_with_default = Entry.objects.aggregate(
+    #        average_rating2=Avg('rating', default=5.0))
+    #   print(average_rating_with_default)
+    # average_rating = Entry.objects.aggregate(
+    #   average_rating3=Avg('rating', filter=Q(pub_date__year__gt=2023)))
+    # print(average_rating)
+    #    count_authors = Entry.objects.aggregate(
+    #        count_authors=Count('author', distinct=True))
+    # print(count_authors)
+    # entries_with_tags_count = Entry.objects.annotate(
+    #        tag_count=Count('tags')).values('id', 'tag_count')
+    #    print(entries_with_tags_count)
+    #    calc_rating = Entry.objects.aggregate(
+    #        max_rating=Max('rating'), min_rating=Min('rating'), avg_rating=Avg('rating'))
+    # print(calc_rating)
+    #    calc_rating = Entry.objects.aggregate(
+    #        std_rating=StdDev('rating'), var_rating=Variance('rating'))
+    # print(calc_rating)
+    #    calc_rating = Entry.objects.aggregate(
+    #        sum_comments=Sum('number_of_comments'))
+    # print(calc_rating)
+    # filtered_data = Blog.objects.filter(id__gte=2)#.order_by("id")
+    # print(filtered_data)
+    # print(filtered_data.reverse())
+    # print(Entry.objects.order_by('author', 'pub_date').distinct('author', 'pub_date'))
+    # print(Blog.objects.filter(name__startswith='Фитнес').values())
+    # items = Blog.objects.values('id','name')
+    # for _ in items:
+    #        print(_)
+    # print(Blog.objects.values_list('id', 'name'))
+    # blog_a_entries = Entry.objects.filter(blog__name='Путешествия по миру')
+    # blog_b_entries = Entry.objects.filter(blog__name='Кулинарные искушения')
+    # blog_c_entries = Entry.objects.filter(blog__name='Фитнес и здоровый образ жизни')
+    # result_qs = blog_a_entries.union(blog_b_entries, blog_c_entries)
+    # qs_1 = Entry.objects.filter(blog__name__in=['Путешествия по миру', 'Кулинарные искушения', 'Фитнес и здоровый образ жизни'])
+    # blog_a_entries = Entry.objects.filter(blog__name='Путешествия по миру').values('author')
+    # blog_b_entries = Entry.objects.filter(blog__name='Кулинарные искушения').values('author')
+    # blog_c_entries = Entry.objects.filter(blog__name='Фитнес и здоровый образ жизни').values('author')
+    # qs_1 = blog_a_entries.intersection(blog_b_entries, blog_c_entries)
+    # print(qs_1)
+    # blog_a_entries = Entry.objects.filter(blog__name='Путешествия по миру').values('author')
+    # blog_b_entries = Entry.objects.filter(blog__name='Кулинарные искушения').values('author')
+    # blog_c_entries = Entry.objects.filter(blog__name='Фитнес и здоровый образ жизни').values('author')
+    # result_qs = Entry.objects.values('author').difference(blog_a_entries, blog_b_entries, blog_c_entries)
+    # print(result_qs)
+    # print(Author.objects.filter(entries__author=None))
+    # entry = Entry.objects.select_related('blog').get(id=5)
+    # blog = entry.blog
+    # print("Число запросов = ", len(connection.queries), " Запросы = ", connection.queries)
+    # print('Результатзапроса = ', blog)
+    class ModelA(models.Model):
+
+
+    # Поля модели
+
+    class ModelB(models.Model):
+        model_a = models.ForeignKey(ModelA, on_delete=models.CASCADE)
+        # Поля модели
+
+
+    class ModelC(models.Model):
+        model_b = models.ForeignKey(ModelB, on_delete=models.CASCADE)
+        # Поля модели
+
+
+    class ModelD(models.Model):
+        model_c = models.ForeignKey(ModelC, on_delete=models.CASCADE)
+        # Поля модели
 
