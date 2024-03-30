b610c660599f3d11f097bd1aed983d8d96b15c24
Alexey <alexeygryzilov@gmail.com>
2024-03-30 15:56:30 +0300

    Lab_2

--- apps/db_train/views.py
+++ apps/db_train/views.py
@@ -10,15 +10,19 @@ class TrainView(View):
         self.answer1 = Author.objects.filter(self_esteem=max_self_esteem['max_self_esteem'])
         self.answer2 = None  # TODO Какой автор имеет наибольшее количество опубликованных статей?
         self.answer3 = None  # TODO Какие статьи содержат тег 'Кино' или 'Музыка' ?
-        self.answer4 = None  # TODO Сколько авторов женского пола зарегистрировано в системе?
-        self.answer5 = None  # TODO Какой процент авторов согласился с правилами при регистрации?
-        self.answer6 = None  # TODO Какие авторы имеют стаж от 1 до 5 лет?
-        self.answer7 = None  # TODO Какой автор имеет наибольший возраст?
-        self.answer8 = None  # TODO Сколько авторов указали свой номер телефона?
-        self.answer9 = None  # TODO Какие авторы имеют возраст младше 25 лет?
+        self.answer4 = Author.objects.filter(gender='ж').count()
+        total = Author.objects.all().count()
+        agree = Author.objects.filter(status_rule__exact=1).count()
+        self.answer5 = int(agree / total * 100)  # TODO Какой процент авторов согласился с правилами при регистрации?
+        self.answer6 = Author.objects.filter(authorprofile__stage__gte=1).filter(authorprofile__stage__lte=5)
+        max_age = Author.objects.aggregate(max_age=Max('age'))
+        self.answer7 = Author.objects.filter(age=max_age['max_age']).values('age')[0]['age']
+        self.answer8 = Author.objects.filter(phone_number__isnull=False).count()
+        self.answer9 = Author.objects.filter(age__lt=25)
         self.answer10 = None  # TODO Сколько статей написано каждым автором?
 
         context = {f'answer{index}': self.__dict__[f'answer{index}'] for index in range(1, 11)}
+        print(context)
 
         return render(request, 'train_db/training_db.html', context=context)
 
